a160f0b1bb2d7808d486a86b082b07e19b215495
DavidSantos004 <ds0299646@gmail.com>
2024-04-01 12:52:33 -0500

    feat: :sparkles: Repositorios Servicio y Seguridad
    Se implemento totalmente seguridad y swagger falta hacer servicio y repositorio

---
+++ src/main/java/com/example/ContenidosAudiovisuales/domain/repository/PlataformaRepository.java
@@ -0,0 +1,5 @@
+package com.example.ContenidosAudiovisuales.domain.repository;
+
+public class PlataformaRepository {
+
+}
---
+++ src/main/java/com/example/ContenidosAudiovisuales/domain/service/UsuarioService.java
@@ -0,0 +1,5 @@
+package com.example.ContenidosAudiovisuales.domain.service;
+
+public class UsuarioService {
+
+}
